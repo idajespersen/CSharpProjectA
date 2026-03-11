112f666dc87d0b2ede5c23c379dd283701ac29ee
idajespersen <idajespersen92@gmail.com>
2026-03-11 11:29:12 +0100

    Added weather forecast output

--- Assignment_A1_01/Program.cs
+++ Assignment_A1_01/Program.cs
@@ -6,15 +6,21 @@ namespace Assignment_A1_01;
 class Program
 {
     static async Task Main(string[] args)
-    {
-        // Pushed code to GitHub
-        double latitude = 59.5086798659495;
-        double longitude = 18.2654625932976;
+    {        
+        double latitude = 60.674855407867405;
+        double longitude = 17.14161302547869;
 
         Forecast forecast = await new OpenWeatherService().GetForecastAsync(latitude, longitude);
 
         //Your Code to present each forecast item in a grouped list
-        Console.WriteLine($"Weather forecast for {forecast.City}");
+        foreach (var i in forecast.Items)
+        {
+            Console.WriteLine($"City: {forecast.City}");
+            Console.WriteLine($"Time: {i.DateTime}");
+            Console.WriteLine($"Temp: {i.Temperature}°C");
+            Console.WriteLine($"Wind: {i.WindSpeed} m/s");
+            Console.WriteLine($"Desc: {i.Description}");
+        }
     }
 }
 
--- Assignment_A1_01/Services/OpenWeatherService.cs
+++ Assignment_A1_01/Services/OpenWeatherService.cs
@@ -6,7 +6,7 @@ namespace Assignment_A1_01.Services;
 public class OpenWeatherService
 {
     HttpClient _httpClient = new HttpClient();
-    readonly string _apiKey = "your_api_key_here"; // Replace with your OpenWeatherMap API key
+    readonly string _apiKey = "6caf4b6aeb0ad514cde32d2296441541"; // Replace with your OpenWeatherMap API key
 
     public async Task<Forecast> GetForecastAsync(double latitude, double longitude)
     {
@@ -19,10 +19,11 @@ public class OpenWeatherService
         
         //Convert Json to NewsResponse
         string content = await response.Content.ReadAsStringAsync();
+
         WeatherApiData wd = JsonConvert.DeserializeObject<WeatherApiData>(content);
 
         //Convert WeatherApiData to Forecast using Linq.
-        //Your code
+        //Your code       
         //Hint: you will find 
         //City: wd.city.name
         //Daily forecast in wd.list, in an item in the list
@@ -32,9 +33,27 @@ public class OpenWeatherService
         //      Description:  first item in weather[].description
         //      Icon:  $"http://openweathermap.org/img/w/{wdle.weather.First().icon}.png"   //NOTE: Not necessary, only if you like to use an icon
 
-        var forecast = new Forecast(); //dummy to compile, replaced by your own code
+        var forecast = new Forecast()
+        {
+            City = wd.city.name,
+            // make new item. use only first index which is current time
+            // item needs to go into 'Items' list somehow...
+            Items = wd.list.Take(1).Select
+            (
+                item => new ForecastItem
+                {
+                    DateTime = UnixTimeStampToDateTime(item.dt),
+                    Temperature = item.main.temp,
+                    WindSpeed = item.wind.speed,
+                    // weather is a list... grab first index
+                    Description = item.weather.First().description
+                }
+            ).ToList()
+        };      
+        
         return forecast;
     }
     private DateTime UnixTimeStampToDateTime(double unixTimeStamp) => DateTime.UnixEpoch.AddSeconds(unixTimeStamp).ToLocalTime();
 }
 
+                                   
